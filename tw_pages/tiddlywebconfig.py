8fb069113d17f71bac4c078ec9af2877c8c7f0ca
bengillies <bengillies@gmail.com>
2009-10-13 00:00:07 +0100

    updated config and readme to reflect addition of filters

--- tw_pages/tiddlywebconfig.py
+++ tw_pages/tiddlywebconfig.py
@@ -1,5 +1,9 @@
 config = {
-        'system_plugins': ['tw_pages'],
-        #this line is optional and assumes templates bag is 'templates', urls bag is 'urls' and config tiddler is bags/config/tiddlers/TWPagesConfig:
-        'tw_pages': {'templates': 'templates', 'urls': 'urls', 'config': ['config', 'TWPagesConfig']}
+        'system_plugins': ['tiddlywebpages'],
+        'tw_pages': {
+            'templates': 'templates',
+            'urls': 'urls',
+            'config': ['config', 'TWPagesConfig'],
+            'filters': []
+            }
 }
