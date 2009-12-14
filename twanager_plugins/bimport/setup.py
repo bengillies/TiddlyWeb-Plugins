2e95c514a51bdd5e8265207ae3bfce9a3000af72
bengillies <bengillies@gmail.com>
2009-12-14 16:22:08 +0000

    turned bimport into a package to be installed as tiddlywebplugins.bimport

---
+++ twanager_plugins/bimport/setup.py
@@ -0,0 +1,27 @@
+# YOU NEED TO EDIT THESE
+AUTHOR = 'Ben Gillies'
+AUTHOR_EMAIL = 'bengillies@gmail.com'
+NAME = 'tiddlywebplugins.bimport'
+DESCRIPTION = 'Import binary content into TiddlyWeb via twanager'
+VERSION = '0.1'
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+# You should carefully review the below (install_requires especially).
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    packages = find_packages(exclude='test'),
+    author_email = AUTHOR_EMAIL,
+    platforms = 'Posix; MacOS X; Windows',
+    install_requires = ['setuptools', 'tiddlyweb']
+    )
