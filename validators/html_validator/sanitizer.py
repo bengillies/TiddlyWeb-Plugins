97cce59ee8dbe1b542cf10e78739f85230495a72
bengillies <bengillies@gmail.com>
2010-01-20 13:57:31 +0000

    attempt to use html5lib for sanitizing instead of BeautifulSoup. Currently the sanitization strips out <html> tags when we want to keep them, so needs a bit more work

--- validators/html_validator/sanitizer.py
+++ validators/html_validator/sanitizer.py
@@ -6,11 +6,13 @@ and allowed_attributes.
 
 These can be specified in tiddlywebconfig.py.
 """
-from tiddlyweb.web.validator import TIDDLER_VALIDATORS
-from tiddlyweb.model.tiddler import Tiddler
-
 import html5lib
 from html5lib.sanitizer import HTMLSanitizer
+from html5lib.tokenizer import HTMLTokenizer
+from html5lib.constants import tokenTypes
+from html5lib.treebuilders import getTreeBuilder
+import re
+from xml.sax.saxutils import escape, unescape
 
 class HTMLValidator(HTMLSanitizer):
     allowed_elements=['html', 'p', 'i', 'strong', 'b', 'u', 'a', 'h1',
@@ -19,7 +21,6 @@ class HTMLValidator(HTMLSanitizer):
         'tr', 'td', 'ol', 'ul', 'li', 'div']
     
     allowed_attributes=['href', 'src', 'alt', 'title']
-    
 
 def update_whitelist(config):
     """
@@ -31,38 +32,18 @@ def update_whitelist(config):
     }
     default to ALLOWED_ELEMENTS and ALLOWED_ATTRIBUTES if none specified
     """
-    if config.get('allowed_elements'):
-        HTMLValidator.allowed_elements = config['allowed_elements']
-    if config.get('allowed_attributes'):
-        HTMLValidator.allowed_attributes = config['allowed_attributes']
+    HTMLValidator.allowed_elements = config.get('allowed_elements', \
+        HTMLValidator.allowed_elements)
+    HTMLValidator.allowed_attributes = config.get('allowed_attributes', \
+        HTMLValidator.allowed_attributes)
 
-def sanitize_html_fragment(fragment):
+def sanitize_html_fragment(fragment, flag=False):
     """
     Sanitize an html fragment, returning a copy of the fragment,
     cleaned up. We use the newly defined Sanitizer
     """
     parser = html5lib.HTMLParser(tokenizer=HTMLValidator)
-    output = parser.parseFragment(fragment)
-    return output.toxml()
-
-def sanitize(tiddler,environ):
-    """
-    pass all fields, tags, text and title into sanitize_html_fragment
-    """
-    print 'text: ', tiddler.text
-    for field,value in tiddler.fields.iteritems():     
-        tiddler.fields[field] = sanitize_html_fragment(value)
-    print 'running text'
-    tiddler.text = sanitize_html_fragment(tiddler.text)      
-    tiddler.tags = map(sanitize_html_fragment,tiddler.tags)
-    tiddler.title = sanitize_html_fragment(tiddler.title)
-    print 'new text: ',tiddler.text
-
-def init(config):
-    """
-    init function
-    """
-    update_whitelist(config)
+    output = parser.parseFragment(fragment, useChardet=False)
     
-    TIDDLER_VALIDATORS.append(sanitize)
-
+    response = output.toxml()
+    return response
